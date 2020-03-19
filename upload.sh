fa8c2356c151924a0e94aa00302708618d4435c1
Mario Botsch <botsch@techfak.uni-bielefeld.de>
2020-03-19 10:04:33 +0100

    add upload script

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# re-build HTML decks
+echo "Build HTML decks"
+decker clean
+decker decks
+#decker pdf-decks
+
+# sync to /vol/cg/www
+echo "Sync ./public/ to /vol/cg/www/"
+rsync --recursive --no-group --no-owner --no-perms --chmod=u=rwX,go=rX --copy-links ./public/ wario.techfak.uni-bielefeld.de:/vol/cg/www/downloads/elearning/
+
+# trigger upload to cg.www
+echo "Upload /vol/cg/www to webserver"
+ssh wario.techfak.uni-bielefeld.de /vol/cg/www/upload.sh
+
